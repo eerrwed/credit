bf52bfe2570027848204331a25192dff39b648c8
eerrwed <45012892+eerrwed@users.noreply.github.com>
2018-11-25 22:24:17 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -499,31 +499,7 @@ client.on('message', message => {
 
 
 
-const adminprefix = "-";
-const devs = ['495397239766908943','228139766573432832'];
-client.on('message', message => {
-  var argresult = message.content.split(` `).slice(1).join(' ');
-    if (!devs.includes(message.author.id)) return;
-    
-if (message.content.startsWith(adminprefix + 'setgame')) {
-  client.user.setGame(argresult);
-} else 
-  if (message.content.startsWith(adminprefix + 'نيم')) {
-client.user.setUsername(argresult).then
-    message.channel.sendMessage(`**${argresult}** : تم تغيير أسم البوت إلى`)
-} else
-  if (message.content.startsWith(adminprefix + 'افتار')) {
-client.user.setAvatar(argresult);
-      } else     
-  if (message.content.startsWith(adminprefix + 'واتش')) {
-  client.user.setActivity(argresult, {type:'WATCHING'});
-  } else
-  if (message.content.startsWith(adminprefix + 'لسن')) {
-  client.user.setActivity(argresult , {type:'LISTENING'});
-  } else    
-if (message.content.startsWith(adminprefix + 'ستريم')) {
-  client.user.setGame(argresult, "https://www.twitch.tv/weeeeented");//py wented
-});
+
 
 
 
