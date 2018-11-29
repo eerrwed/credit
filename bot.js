eb11e7f9a6e61e658a76e4bab9c27afc5322f3b6
eerrwed <45012892+eerrwed@users.noreply.github.com>
2018-11-29 17:06:09 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -185,34 +185,32 @@ client.on("message", message => {
 
   
   
-  client.on('message', message => {
+
+
+client.on('message', message => {
     if (message.content.startsWith("رابط")) {
- 
-  message.channel.createInvite({
+        message.channel.createInvite({
         thing: true,
         maxUses: 5,
-        maxAge: 86400
+        maxAge: 3600,
     }).then(invite =>
       message.author.sendMessage(invite.url)
     )
     const embed = new Discord.RichEmbed()
         .setColor("RANDOM")
-        .setDescription("| تم ارسال الرابط على الخاص ")
+          .setDescription("تم أرسال الرابط برسالة خاصة")
+           .setAuthor(client.user.username, client.user.avatarURL)
+                 .setAuthor(client.user.username, client.user.avatarURL)
+                .setFooter('طلب بواسطة: ' + message.author.tag)
+
       message.channel.sendEmbed(embed).then(message => {message.delete(10000)})
               const Embed11 = new Discord.RichEmbed()
         .setColor("RANDOM")
-                .setAuthor(message.guild.name, message.guild.iconURL)
-        .setDescription(`
-**
----------------------
--[${message.guild.name}]  
-الرابط ل 5 اشخاص لمده ساعه
-**Welcome To Links Team**`)
+        
+    .setDescription("**مدة الرابط : ساعه | عدد استخدامات الرابط : 5 **")
       message.author.sendEmbed(Embed11)
     }
-});
-  
-  
+}); 
 
   
 
