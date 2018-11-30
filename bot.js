74c20fe4bef2022f6bad534dfef69f17470451e6
eerrwed <45012892+eerrwed@users.noreply.github.com>
2018-11-30 20:20:28 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -522,23 +522,41 @@ client.on('message', message => {
 
 
 
-var antispam = require("anti-spam");
- antispam(client, {
-  warnBuffer: 5, //الحد الأقصى المسموح به من الرسائل لإرسالها في الفاصل الزمني قبل الحصول على تحذير.
-  maxBuffer: 6, // الحد الأقصى المسموح به من الرسائل لإرسالها في الفاصل الزمني قبل الحصول على ميوت.
-  interval: 1000, // مقدار الوقت قبل حصول باند
-  warningMessage: "stop spamming.", // رسالة تحذير اذا سوا سبام!
-  roleMessage: "محاولة سبام!!", // الرسالة الي تجي اذا شخص اخذ ميوت
-  roleName: "Muted", // اسم رتبة الميوت
-  maxDuplicatesWarning: 7, // عدد الرسايل الي قبل التحذيرات
-  maxDuplicatesBan: 10, // عدد الرسايل الي يقدر المستخدم يرسلها قبل الميوت
-  time: (10*1000), // عدد الوقت الي يجلس لين تسحب رتبة الميوت من الشخص الحسبة برمجية وليست كتابية 
+client.on('message',async message => {
+    if(message.content.startsWith(prefix + "res")) {
+        if(message.author.id !== "244888652004458497") return message.reply('You aren't the bot owner.');
+        message.channel.send('Restarting.').then(msg => {
+            setTimeout(() => {
+               msg.edit('Restarting..');
+            },1000);
+            setTimeout(() => {
+               msg.edit('Restarting...');
+            },2000);
+        });
+        console.log(${message.author.tag} [ ${message.author.id} ] has restarted the bot.);
+        console.log(Restarting..);
+        setTimeout(() => {
+            client.destroy();
+            client.login('process.env.BOT_TOKEN');
+        },3000);
+    }
 });
 
 
 
-
-
+var antispam = require("anti-spam");//npm i anti-spam
+ 
+antispam(client, {
+  warnBuffer: 3, //الحد الأقصى المسموح به من الرسائل لإرسالها في الفاصل الزمني قبل الحصول على تحذير.
+  maxBuffer: 5, // الحد الأقصى المسموح به من الرسائل لإرسالها في الفاصل الزمني قبل الحصول على ميوت.
+  interval: 1000, // مقدار الوقت قبل حصول باند
+  warningMessage: "stop spamming.", // رسالة تحذير اذا سوا سبام!
+  roleMessage: "Muted!!", // الرسالة الي تجي اذا شخص اخذ ميوت
+  roleName: "Muted", // اسم رتبة الميوت
+  maxDuplicatesWarning: 7, // عدد الرسايل الي قبل التحذيرات
+  maxDuplicatesBan: 10, // عدد الرسايل الي يقدر المستخدم يرسلها قبل الميوت
+  time: (10*20000), // عدد الوقت الي يجلس لين تسحب رتبة الميوت من الشخص الحسبة برمجية وليست كتابية 
+});
 
 
 
